d7877629af92429aee2a805017e2e653db2aa60a
Mike Sosa <mcs348@cornell.edu>
2017-10-01 12:36:29 -0400

    First steps

--- HiltonHackathon/app/src/main/java/com/cornell/wbncjm/hiltonhackathon/Home.java
+++ HiltonHackathon/app/src/main/java/com/cornell/wbncjm/hiltonhackathon/Home.java
@@ -1,12 +1,43 @@
 package com.cornell.wbncjm.hiltonhackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.home);
+
+    }
+
+    /**
+     * Log in Button
+     */
+    public void logIn(View view) {
+        Intent intent = new Intent(this, LogIn.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+    }
+
+    /**
+     * Create account btton
+     */
+    public void createAccount(View view) {
+        //No
     }
 }
