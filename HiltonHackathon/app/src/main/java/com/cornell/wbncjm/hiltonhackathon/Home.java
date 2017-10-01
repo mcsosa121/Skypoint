bbd0758dab4c8869ade8228ceb3f1b3b5027e6bf
Mike Sosa <mcs348@cornell.edu>
2017-09-30 23:48:40 -0400

    ???

--- HiltonHackathon/app/src/main/java/com/cornell/wbncjm/hiltonhackathon/Home.java
+++ HiltonHackathon/app/src/main/java/com/cornell/wbncjm/hiltonhackathon/Home.java
@@ -3,11 +3,10 @@ package com.cornell.wbncjm.hiltonhackathon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
     }
 }
